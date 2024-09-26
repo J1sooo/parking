7cad9328cce70703cdaa4c010811de33f1334535
j1sooo <kjisoo@gmail.com>
2024-09-26 15:58:00 +0900

    frontend 및 tailwindcss 기본 설정

--- parking_frontend/src/App.js
+++ parking_frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from "./page/Main";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactaaaasdfafd
-        </a>
-      </header>
+    <div className="page">
+      <Routes>
+        <Route path="/" element={<Main/>} />
+      </Routes>
     </div>
   );
 }
---
+++ parking_frontend/src/page/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Main() {
+
+    return (
+        <div>
+            <button className="scale-75 ...">
+                Save changes
+            </button>
+        </div>
+    )
+}
+export default Main;
