8e350a12767233787e4bb35332b7a377cf156b3c
j1sooo <kjisoo@gmail.com>
2024-10-27 09:57:24 +0900

    재귀함수로 while문 삭제

--- parking_backend/src/main/java/com/shingu/api/kakaoapi/kakaoapiService.java
+++ parking_backend/src/main/java/com/shingu/api/kakaoapi/kakaoapiService.java
@@ -22,10 +22,11 @@ public class kakaoapiService {
         return response.getBody();
     }
 
-
-
     // 좌표 없는 데이터 좌표 찾기
     public kakaoSearchDto searchAddressXY(String address) {
+        if (address == null || address.isEmpty()) {
+            return null;
+        }
         String apiUrl = "https://dapi.kakao.com/v2/local/search/address.json?query="+address;
         kakaoSearchDto result = kakaoApiRequest(apiUrl);
 
@@ -33,18 +34,16 @@ public class kakaoapiService {
         if (result == null || result.getDocuments().isEmpty()) {
             address = address.replaceAll(" ",""); // 공백 제거
             // 주소에는 무조건 읍,면,동,리가 들어가므로 무한루프 X
-            while (true){
-                String finalAddress;
-                System.out.println(address);
-                if (address.endsWith("도") || address.endsWith("시") ||
-                    address.endsWith("군") || address.endsWith("구") ||
-                    address.endsWith("읍") || address.endsWith("면") ||
-                    address.endsWith("동") || address.endsWith("리")) {
-                    break;
-                } else {
-                    finalAddress = address.substring(0, address.length() - 1);
-                    return searchAddressXY(finalAddress);
-                }
+            String finalAddress;
+            System.out.println(address);
+            if (address.endsWith("도") || address.endsWith("시") ||
+                address.endsWith("군") || address.endsWith("구") ||
+                address.endsWith("읍") || address.endsWith("면") ||
+                address.endsWith("동") || address.endsWith("리")) {
+                return searchAddressXY(null);
+            } else {
+                finalAddress = address.substring(0, address.length() - 1);
+                return searchAddressXY(finalAddress);
             }
         }
         return result;
@@ -60,5 +59,10 @@ public class kakaoapiService {
             result = kakaoApiRequest(apiUrl);
         }
         return result;
+    }
+
+    public kakaoSearchDto userLocation(Double Ylat, Double Xlon){
+        String apiUrl = "https://dapi.kakao.com/v2/local/geo/coord2address.json?y="+Ylat+"&x="+Xlon;
+        return kakaoApiRequest(apiUrl);
     }
 }
